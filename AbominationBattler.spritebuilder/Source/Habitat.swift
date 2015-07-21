46c47eff3954ec695d2055ca93513f54849c2d4a
Laura Bennett <falconoflight@gmail.com>
2015-07-21 13:28:55 -0700

    health and happiness bars, happiness bar broken

--- AbominationBattler.spritebuilder/Source/Habitat.swift
+++ AbominationBattler.spritebuilder/Source/Habitat.swift
@@ -10,15 +10,55 @@
 
 class Habitat: CCNode {
     
+    //weak var Character: CCSprite!
+    
+    weak var hungerBar: CCSprite!
+    weak var happyBar: CCSprite!
+    weak var happyButton: CCButton!
+    weak var feedButton: CCButton!
+    
+    var timeLeft: Float = 1000 {
+        didSet {
+            timeLeft = max(min(timeLeft, 1000), 0)
+            hungerBar.scaleX = timeLeft / Float(1000)
+        }
+    }
+    
+    var boredom: Float = 1000 {
+        didSet {
+            boredom = max(min(boredom, 1000),0)
+            happyBar.scaleX = timeLeft / Float(1000)
+        }
+    }
+    
+    func didLoadFromCCB() {
+        
+        userInteractionEnabled = true
+    }
+    
+    override func update(delta: CCTime) {
+        boredom -= Float(delta)
+        
+        timeLeft -= Float(delta)
+        if timeLeft == 0 {
+            //tamagachi dies
+            println("gameOver")
+        }
+    }
+    
+    func feed() {
+        timeLeft = 1000
+    }
+
+    
     func toArena() {
         let toArena = CCBReader.loadAsScene("BattleArena")
         CCDirector.sharedDirector().presentScene(toArena)
     }
     
-    //func happyUp() {
-     //   let habitat = CCBReader.loadAsScene("Habitat")
-     //   CCDirector.sharedDirector().presentScene(habitat)
-    //}
+    func happyUp() {
+        boredom = 1000
+    }
     
     //func stats() {
       //  let habitat = CCBReader.loadAsScene("Habitat")
