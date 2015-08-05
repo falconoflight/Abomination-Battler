7c43a3bf9c5841953833a2a7c1a76a6afb9af3e1
Laura Bennett <falconoflight@gmail.com>
2015-08-05 13:26:56 -0700

    play button?

--- AbominationBattler.spritebuilder/Source/MainScene.swift
+++ AbominationBattler.spritebuilder/Source/MainScene.swift
@@ -2,34 +2,43 @@ import Foundation
 
 class MainScene: CCNode {
     
-    weak var continueButton: CCButton!
-    weak var newGameButton: CCButton!
+//    weak var continueButton: CCButton!
+//    weak var newGameButton: CCButton!
+    weak var playButton: CCButton!
     
-    
-    func play() {
-        let habitat = CCBReader.loadAsScene("Habitat")
-        CCDirector.sharedDirector().presentScene(habitat)
-    }
-    
-    func newGame() {
-        let newGame = CCBReader.loadAsScene("NewGame")
-        CCDirector.sharedDirector().presentScene(newGame)
-    
-    }
+//    func play() {
+//        let habitat = CCBReader.loadAsScene("Habitat")
+//        CCDirector.sharedDirector().presentScene(habitat)
+//    }
+//    
+//    func newGame() {
+//        let newGame = CCBReader.loadAsScene("NewGame")
+//        CCDirector.sharedDirector().presentScene(newGame)
+//    
+//    }
 
     
     override func onEnter() {
         super.onEnter()
         var userState = UserState()
         println(userState.name + " " + userState.monsterType + " \(userState.didSummonMonster)")
-//        
-//        if userState.didSummonMonster {
+        
+        if userState.didSummonMonster {
 //            newGameButton.visible = false
-//        }
-//        
-//        else {
+                let habitat = CCBReader.loadAsScene("Habitat")
+                CCDirector.sharedDirector().presentScene(habitat)
+
+        }
+        
+        else {
 //            continueButton.visible = false
-//        }
+//            func newGame() {
+                let newGame = CCBReader.loadAsScene("NewGame")
+                CCDirector.sharedDirector().presentScene(newGame)
+                
+//            }
+
+        }
     }
 
 }
