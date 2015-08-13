5d7db8a9c120333f107eff70cc244524e9f3fd6f
Laura Bennett <falconoflight@gmail.com>
2015-08-13 16:54:40 -0700

    created a mix panel class.

---
+++ AbominationBattler.spritebuilder/MixPanel.swift
@@ -0,0 +1,12 @@
+//
+//  MixPanel.swift
+//  AbominationBattler
+//
+//  Created by Carol on 8/13/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+class MixPanel {
+    
+}
